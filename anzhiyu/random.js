cc253a308a2e7b68b4b527eff6b086d60a1d6866
xinhanyuan <1056817261@qq.com>
2026-01-29 15:06:16 +0800

    Site updated: 2026-01-29 15:06:15

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/01/21/20260120-Snow/","2026/01/19/20260119-FirstBlog/","2026/01/27/20260127-Docker/"];function toRandomPost(){
+var posts=["2026/01/19/20260119-FirstBlog/","2026/01/21/20260120-Snow/","2026/01/27/20260127-Docker/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
